07d933890f70d1229815d775b123b2a8c84cf2df
Julio Errecart <julioer@me.com>
2023-12-03 11:09:41 -0300

    Add cleaner version of search method

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,31 +2,24 @@ class StocksController < ApplicationController
 
   def search
 
+    # If a ticker was sent, search for the stock
     if params[:stock].present?
-      # Ingresó un ticker en la pagina - busco el stock
+
       @stock = Stock.new_lookup(params[:stock])
 
-      if @stock
-        # El ticker era valido y el metodo devolvio un objeto "Stock"
-        respond_to do | format |
-          format.js { render partial: 'users/result_js' } 
-        end
-      else
-        # El ticker no era valido y el metodo devolvio "null"
-        respond_to do | format |
-          flash.now[:alert] = "Invalid symbol. Please enter a valid symbol to search"
-          format.js { render partial: 'users/result_js' }
-        end
+      # If it was an invalid ticker...
+      if !@stock
+        flash.now[:alert] = "Invalid entry. Please enter a valid symbol to search"
       end
-
     else
-      # No ingresó nada en la pagina
-      respond_to do | format |
         flash.now[:alert] = "Please enter a symbol to search"
-        format.js { render partial: 'users/result_js' }
-      end
     end
 
+    # AJAX Response - Renders the partial in both success and error scenarios
+    #                 sending either a valid Stock or an error message 
+    respond_to do | format |
+      format.js { render partial: 'users/result_js' }
+    end
   end
 
 end
