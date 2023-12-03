6a696fdac7230c26f740156d256f41821aabba37
Julio Errecart <julioer@me.com>
2023-12-03 10:40:56 -0300

    Add AJAX error handling

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,19 +1,32 @@
 class StocksController < ApplicationController
+
   def search
+
     if params[:stock].present?
+      # Ingresó un ticker en la pagina - busco el stock
       @stock = Stock.new_lookup(params[:stock])
 
       if @stock
+        # El ticker era valido y el metodo devolvio un objeto "Stock"
         respond_to do | format |
-          format.js { render partial: 'users/result_js' }
+          format.js { render partial: 'users/result_js' } 
         end
       else
-        flash[:alert] = "Invalid symbol. Please enter a valid symbol to search"
-        redirect_to my_portfolio_path
+        # El ticker no era valido y el metodo devolvio "null"
+        respond_to do | format |
+          flash.now[:alert] = "Invalid symbol. Please enter a valid symbol to search"
+          format.js { render partial: 'users/result_js' }
+        end
       end
+
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portfolio_path
+      # No ingresó nada en la pagina
+      respond_to do | format |
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js { render partial: 'users/result_js' }
+      end
     end
+
   end
+
 end
