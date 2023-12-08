3d6dc237a00b6b09855dad9f7f0bb1f2a7116a84
Julio Errecart <julioer@me.com>
2023-12-08 14:16:10 -0300

    Add friends search form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
+
+  def search
+    @friend = User.find_by(first_name: params[:friend])
+
+    if @friend
+      render json: @friend 
+    else     
+      render json: params[:friend] 
+    end  
+  end
+  
 end
